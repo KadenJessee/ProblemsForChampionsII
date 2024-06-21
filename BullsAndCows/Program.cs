a7e780afa2caa0b46a59a70dafece1e4ce04161e
Kaden Jessee <kjessee721@gmail.com>
2024-06-21 12:04:21 -0600

    recursion and arrays/lists with bulls and cows

--- BullsAndCows/Program.cs
+++ BullsAndCows/Program.cs
@@ -4,133 +4,27 @@
     {
         static void Main(string[] args)
         {
-            int guessNum = int.Parse(Console.ReadLine());
+            string guessNum = Console.ReadLine();
             int targetBulls = int.Parse(Console.ReadLine());
             int targetCows = int.Parse(Console.ReadLine());
 
+            List<string> results = new List<string>();
+            GenerateCombinations(new char[guessNum.Length], 0, results, guessNum.Length);
+
             bool found = false;
 
-
-            for (int digit1 = 1; digit1 <= 9; digit1++)
+            foreach (var combination in results)
             {
-                for (int digit2 = 1; digit2 <= 9; digit2++)
+                var (currBulls, currCows) = CalculateBullsAndCows(guessNum, combination);
+
+                if (currBulls == targetBulls && currCows == targetCows)
                 {
-                    for (int digit3 = 1; digit3 <= 9; digit3++) 
+                    if (found)
                     {
-                        for (int digit4 = 1; digit4 <= 9; digit4++)
-                        {
-                            int guessDigit1 = (guessNum / 1000) % 10;
-                            int guessDigit2 = (guessNum / 100) % 10;
-                            int guessDigit3 = (guessNum / 10) % 10;
-                            int guessDigit4 = (guessNum / 1) % 10;
-
-                            int digitToCheck1 = digit1;
-                            int digitToCheck2 = digit2;
-                            int digitToCheck3 = digit3;
-                            int digitToCheck4 = digit4;
-
-                            int currBulls = 0;
-                            int currCows = 0;
-
-                            //counting bulls
-                            if(digitToCheck1 == guessDigit1)
-                            {
-                                currBulls++;
-                                guessDigit1 = -1;
-                                digitToCheck1 = -2;
-                            }
-                            if (digitToCheck2 == guessDigit2)
-                            {
-                                currBulls++;
-                                guessDigit2 = -1;
-                                digitToCheck2 = -2;
-                            }
-                            if (digitToCheck3 == guessDigit3)
-                            {
-                                currBulls++;
-                                guessDigit3 = -1;
-                                digitToCheck3 = -2;
-                            }
-                            if (digitToCheck4 == guessDigit4)
-                            {
-                                currBulls++;
-                                guessDigit4 = -1;
-                                digitToCheck4 = -2;
-                            }
-
-                            //counting cows
-                            if (digitToCheck1 == guessDigit2)
-                            {
-                                currCows++;
-                                guessDigit2 = -1;
-                            }
-                            else if (digitToCheck1 == guessDigit3)
-                            {
-                                currCows++;
-                                guessDigit3 = -1;
-                            }
-                            else if (digitToCheck1 == guessDigit4)
-                            {
-                                currCows++;
-                                guessDigit4 = -1;
-                            }
-                            if (digitToCheck2 == guessDigit1)
-                            {
-                                currCows++;
-                                guessDigit1 = -1;
-                            }
-                            else if (digitToCheck2 == guessDigit3)
-                            {
-                                currCows++;
-                                guessDigit2 = -1;
-                            }
-                            else if (digitToCheck2 == guessDigit4) 
-                            {
-                                currCows++;
-                                guessDigit4 = -1;
-                            }
-                            if(digitToCheck3 == guessDigit1)
-                            {
-                                currCows++;
-                                guessDigit1 = -1;
-                            }
-                            else if( digitToCheck3 == guessDigit2)
-                            {
-                                currCows++;
-                                guessDigit2 = -1;
-                            }
-                            else if(digitToCheck3 == guessDigit4)
-                            {
-                                currCows++;
-                                guessDigit4 = -1;
-                            }
-                            if(digitToCheck4 == guessDigit1)
-                            {
-                                currCows++;
-                                guessDigit1 = -1;
-                            }
-                            else if(digitToCheck4== guessDigit2)
-                            {
-                                currCows++;
-                                guessDigit2 = -1;
-                            }
-                            else if(digitToCheck4 == guessDigit3)
-                            {
-                                currCows++;
-                                guessDigit3 = -1;
-                            }
-
-                            if (currBulls == targetBulls && currCows == targetCows) 
-                            {
-                                if (found)
-                                {
-                                    Console.Write(" ");
-                                }
-                                Console.Write($"{digit1}{digit2}{digit3}{digit4}");
-                                found = true;
-                            }
-                        }
+                        Console.Write(" ");
                     }
+                    Console.Write(combination);
+                    found = true;
                 }
             }
 
@@ -138,6 +32,58 @@
             {
                 Console.WriteLine("No");
             }
+        }
+
+        static void GenerateCombinations(char[] current, int index, List<string> results, int length)
+        {
+            if (index == length)
+            {
+                results.Add(new string(current));
+                return;
+            }
+
+            for (char digit = '1'; digit <= '9'; digit++)
+            {
+                current[index] = digit;
+                GenerateCombinations(current, index + 1, results, length);
+            }
+        }
+
+        static (int bulls, int cows) CalculateBullsAndCows(string guess, string combination)
+        {
+            int bulls = 0, cows = 0;
+            bool[] guessUsed = new bool[guess.Length];
+            bool[] combinationUsed = new bool[combination.Length];
+
+            // Count bulls
+            for (int i = 0; i < guess.Length; i++)
+            {
+                if (guess[i] == combination[i])
+                {
+                    bulls++;
+                    guessUsed[i] = true;
+                    combinationUsed[i] = true;
+                }
+            }
+
+            // Count cows
+            for (int i = 0; i < guess.Length; i++)
+            {
+                if (!guessUsed[i])
+                {
+                    for (int j = 0; j < combination.Length; j++)
+                    {
+                        if (!combinationUsed[j] && guess[i] == combination[j])
+                        {
+                            cows++;
+                            combinationUsed[j] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return (bulls, cows);
         }
     }
 }
